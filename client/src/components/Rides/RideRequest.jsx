da0eacf62823a8664b061d5d18ad93dd97d5e9cf
baronchibuikem <anozie.uchechukwu@e4email.net>
2021-02-22 08:00:10 +0100

    update: Added react and typescript

--- client/src/components/Rides/RideRequest.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { webSocket } from "rxjs/webSocket";
-import { Redirect } from "react-router-dom";
-
-function RideRequest() {
-  return <div></div>;
-}
-
-export default RideRequest;
